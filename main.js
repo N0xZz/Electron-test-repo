7f1e8e8818c90c98bd0a635bf5bd480e8e3b366f
N0xZz <max-gramberg@gmx.net>
2022-09-22 17:41:44 +0200

    merged Kay's xlsx to json convert logic

--- main.js
+++ main.js
@@ -1,14 +1,56 @@
+// Modules to control application life and create native browser window
 const { app, BrowserWindow } = require('electron')
+const path = require('path')
+const os = require('os')
 
-const createWindow = () => {
-    const win = new BrowserWindow({
-      width: 800,
-      height: 600
-    })
-  
-    win.loadFile('index.html')
+// set env
+process.env.NODE_ENV = 'development'
+
+const isDev = process.env.NODE_ENV !== 'production' ? true : false
+const isMac = process.platform === 'darwin' ? true : false
+
+function createWindow () {
+  // Create the browser window.
+  const mainWindow = new BrowserWindow({
+    width: isDev ? 1280 : 600,
+    height: 400,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      nodeIntegration: true,
+      contextIsolation: true, // add this
+    }
+  })
+
+  if (isDev) {
+    mainWindow.webContents.openDevTools()
+  }
+
+  // and load the index.html of the app.
+  mainWindow.loadFile('index.html')
+  mainWindow.setMenuBarVisibility(false)
+  // Open the DevTools.
+  // mainWindow.webContents.openDevTools()
 }
 
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
-    createWindow()
+  createWindow()
+
+  app.on('activate', function () {
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
 })
+
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit()
+})
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and require them here.
---
+++ preload.js
@@ -0,0 +1,50 @@
+// All of the Node.js APIs are available in the preload process.
+// It has the same sandbox as a Chrome extension.
+
+var node_xj = require("xls-to-json");
+
+
+window.addEventListener('DOMContentLoaded', () => {
+  const replaceText = (selector, text) => {
+    const element = document.getElementById(selector)
+    if (element) element.innerText = text
+  }
+
+  for (const type of ['chrome', 'node', 'electron']) {
+    replaceText(`${type}-version`, process.versions[type])
+  }
+  const dropzone = document.getElementById("dropzone");
+ console.log(dropzone);
+
+
+ document.body.addEventListener("dragover", evt => {  /*This is necessary otherwise the 'drop' listener wont work */
+  evt.preventDefault();
+});
+
+dropzone.addEventListener("drop", (e) =>{
+  e.stopPropagation();
+  e.preventDefault();
+
+  const files = e.dataTransfer.files;
+    
+    for (const file of files){
+      
+      node_xj(
+        {
+          input: file.path,
+          output: file.path.split('.')[0] + ".json",
+          sheet: "MediaAssets_Master_new",
+          allowEmptyKey: false,
+        },
+        function (err,result) {
+          if(err){console.log(err);}
+        }
+      )
+     }
+  })
+})
+
+
+
+
+
